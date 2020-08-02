ba1546d698a32a7bc6090085d262cb6922432a3e
rbaron <rbaron@rbaron.net>
2020-08-02 17:46:13 +0200

    Quick lexer

---
+++ include/lexer.h
@@ -0,0 +1,94 @@
+#ifndef _LEXER_H_
+#define _LEXER_H_
+
+#include <cctype>
+#include <optional>
+#include <vector>
+#include <string>
+#include <string_view>
+
+namespace minijson {
+
+enum class TokenType {
+  kLCurlyBracket,
+  kRCurlyBracket,
+  kLSquareBracket,
+  kRSquareBracket,
+  kName, // true, false, null
+  kStr,
+  kComma,
+  kColon,
+  kNumber,
+};
+
+struct Token {
+  TokenType type;
+  std::string text;
+};
+
+namespace {
+
+Token TokenizeString(std::string::const_iterator *it) {
+  // TODO: handle escaped double quotes
+  auto start = *it;
+  while (*(++*it) != '"');
+  return Token{TokenType::kStr, std::string(start+1, (*it)++)};
+}
+
+Token TokenizeNumber(std::string::const_iterator *it) {
+  auto start = *it;
+  while (isdigit(*(++*it)));
+  return Token{TokenType::kNumber, std::string(start, *it)};
+}
+
+bool IsNameChar(char c) {
+  return isalnum(c) || c == '_' || c == '-';
+}
+
+Token TokenizeName(std::string::const_iterator *it) {
+  auto start = *it;
+  while (IsNameChar(*++*it));
+  return Token{TokenType::kName, std::string(start, *it)};
+}
+
+std::optional<Token> TonekizeOne(std::string::const_iterator *it) {
+  if (**it == '{') {
+    return Token{TokenType::kLCurlyBracket, std::string(*it, ++*it)};
+  } else if (**it == '}') {
+    return Token{TokenType::kRCurlyBracket, std::string(*it, ++*it)};
+  } else if (**it == '[') {
+    return Token{TokenType::kLSquareBracket, std::string(*it, ++*it)};
+  } else if (**it == ']') {
+    return Token{TokenType::kRSquareBracket, std::string(*it, ++*it)};
+  } else if (**it == ':') {
+    return Token{TokenType::kColon, std::string(*it, ++*it)};
+  } else if (**it == ',') {
+    return Token{TokenType::kComma, std::string(*it, ++*it)};
+  } else if (**it == '"') {
+    return TokenizeString(it);
+  } else if (isdigit(**it)) {
+    return TokenizeNumber(it);
+  } else if (IsNameChar(**it)) {
+    return TokenizeName(it);
+  // Whitespaces, hopefully
+  } else {
+    ++*it;
+    return std::nullopt;
+  }
+}
+
+}  // namespace
+
+std::vector<Token> Tokenize(const std::string& input) {
+  std::vector<Token> tokens;
+  for (auto it = input.begin(); it != input.end(); ) {
+    std::optional<Token> token = TonekizeOne(&it);
+    if (token.has_value()) {
+      tokens.push_back(token.value());
+    }
+  }
+  return tokens;
+}
+
+}  // namespace minijson
+#endif  // _LEXER_H_
--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <string>
+
+#include "lexer.h"
 
 int main(int argc, char **argv) {
-  std::cout << "Hello, world\n";
+  const std::string json = R"(
+    {
+      "hello": "ok",
+      "number_key": 123,
+      "bool_key": true,
+      "list_key": [
+        "item1",
+        321,
+      ],
+      "nested_key": {
+        "nested_key_1": 123,
+        "nested_key_2": "abc",
+      }
+    }
+  )";
+  std::cout << json << std::endl;
+  auto tokens = minijson::Tokenize(json);
+  for (const auto& t : tokens) {
+    std::cout << t.text << std::endl;
+  }
   return 0;
 }
